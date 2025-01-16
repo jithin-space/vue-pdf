eb0c0f86ddd3ca40b25197958aea8b394cee9885
jithin-space <jithin.thankachan@gpsrenewables.com>
2025-01-16 11:57:26 +0530

    viewer-update

--- packages/vue-pdf/src/components/index.ts
+++ packages/vue-pdf/src/components/index.ts
@@ -1,3 +1,5 @@
+export  * from './usePDFViewer';
 export { default as VuePDF } from './VuePDF.vue'
 export * from './composable'
 export * from './types'
+export { default as VuePDFViewer } from './VuePDFViewer.vue';
