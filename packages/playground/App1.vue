c591de8ed7515e1a6320c596dbca075572b9b7bf
jithin-space <jithin.thankachan@gpsrenewables.com>
2025-01-16 11:36:25 +0530

    basic viewer functionality

---
+++ packages/playground/App1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="abs-stretch">
+    <!-- Pass pdfDocument as a prop to the VuePDFViewer component -->
+    <VuePDFViewer :pdf="pdf">
+      <div>
+        <h1>Loading....</h1>
+      </div>
+    </VuePDFViewer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import pdf14 from '@samples/large.pdf';
+import { VuePDFViewer, usePDFViewer} from '@tato30/vue-pdf';
+
+
+const { pdf } = usePDFViewer(pdf14);
+
+
+</script>
+<style lang="css" scoped>
+.abs-stretch {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ packages/vue-pdf/src/components/usePDFViewer.ts
@@ -0,0 +1,36 @@
+// usePdfLoader.ts
+import * as PDFJS from "pdfjs-dist";
+import PDFWorker from "pdfjs-dist/build/pdf.worker.min?url";
+import type { Ref } from "vue";
+import type { PDFSrc } from "./types";
+import { isRef, shallowRef, watch } from "vue";
+import type { PDFDocumentLoadingTask } from "pdfjs-dist";
+
+function configWorker(wokerSrc: string) {
+  PDFJS.GlobalWorkerOptions.workerSrc = wokerSrc;
+}
+
+export function usePDFViewer(src: PDFSrc | Ref<PDFSrc>) {
+  if (!PDFJS.GlobalWorkerOptions?.workerSrc) configWorker(PDFWorker);
+
+  const pdf = shallowRef<PDFDocumentLoadingTask>();
+
+  function processLoadingTask(source: PDFSrc) {
+    pdf.value = PDFJS.getDocument(source!);
+  }
+
+  if (isRef(src)) {
+    if (src.value) {
+      processLoadingTask(src.value);
+    }
+    watch(src, () => {
+      if (src.value) processLoadingTask(src.value);
+    });
+  } else {
+    if (src) processLoadingTask(src);
+  }
+
+  return {
+    pdf,
+  };
+}
